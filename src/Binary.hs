5861c1b9b99cf2ec500d3990e93901e7d43ed61d
Paul Cadman <git@paulcadman.io>
2019-11-09 08:00:59 +0000

    Use data.sequence

--- src/Binary.hs
+++ src/Binary.hs
@@ -1,13 +1,16 @@
 module Binary where
 
+import Data.Sequence
+
 rank :: Int -> [Int] -> Int
-rank key l = rank_aux 0 (len - 1) where
-  len = length l
+rank key a = rank_aux 0 (len - 1) where
+  len = Prelude.length a
   rank_aux lo hi
     | lo > hi = -1
     | key < l_mid = rank_aux lo (mid - 1)
     | key > l_mid = rank_aux (mid + 1) hi
     | otherwise = mid
     where
+      l = fromList a
       mid = lo + ((hi - lo) `div` 2)
-      l_mid = l !! mid
+      l_mid = index l mid
